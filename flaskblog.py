29fd65ce9b67f1fc3f8f6a43dce4c725562c71ee
shankarkarki <shankar.karki660@gmail.com>
2020-12-10 00:12:23 +0545

    templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 app = Flask(__name__)
+
+posts=[
+    {
+        'author':'Shankar Karki',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'April 20,2020'
+    },{
+        'author':'Hera Karki',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'April 21,2020'
+    }
+
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>HOME PAGE!!</h1>"
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>ABOUT PAGE!!</h1>"
+    return render_template('about.html',title='About')
 
 if __name__=='__main__':
     app.run(debug=True)
