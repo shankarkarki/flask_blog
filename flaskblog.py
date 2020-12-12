4103f8d87c01ba6dc5e64eb794322682444d137c
shankarkarki <shankar.karki660@gmail.com>
2020-12-12 13:11:13 +0545

    form validation part 1

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask,render_template,url_for
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='32c049e7e582f7de8d02d7a2b846c8ee'
 
 posts=[
     {
@@ -24,6 +27,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register")
+def register():
+    form=RegistrationForm()
+    return render_template('register.html',title='Register',form=form)   
+
+@app.route("/login")
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login',form=form) 
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
